4e7d340ed9454b8f2a5dab9f7aa7ebeddebf57a5
mwchoen <mwcheon@cudo.co.kr>
2022-03-16 09:06:09 +0900

    TS 적용1단계 - 라우터, HOC

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
-// @ts-ignore
-import Vue from 'vue'
-import App from './App.vue'
-import router from './routes/index.js';
-import store from './store/index.js';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./routes";
+import store from "./store/index.js";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
